5cdc7d20624e878ed72558961a7c601ab4247b7a
arman <alaalfam@gmail.com>
2025-09-24 22:47:30 +0330

    Edit dashboard page that show popular movies in grid view

---
+++ movie_land/lib/core/utils/datetime_converter.dart
@@ -0,0 +1,20 @@
+import 'package:intl/intl.dart';
+
+class DatetimeConverter {
+  String convertToReadableDate(String dateString) {
+    try {
+      final dateTime = DateTime.parse(dateString);
+      return '${dateTime.day}/${dateTime.month}/${dateTime.year}';
+    } catch (e) {
+      return dateString; // Return the original string if parsing fails
+    }
+  }
+
+  String convertToHumanReadable(String dateString) {
+    DateTime parsedDate = DateTime.parse(dateString);
+
+    // Format the date in the required format
+    String formattedDate = DateFormat('dd MMM yyyy').format(parsedDate);
+    return formattedDate;
+  }
+}
--- movie_land/lib/presentation/views/dashboard_page.dart
+++ movie_land/lib/presentation/views/dashboard_page.dart
@@ -1,11 +1,13 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
+import 'package:movie_land/core/utils/datetime_converter.dart';
 import 'package:movie_land/core/utils/movie_land_dimentions.dart';
 import 'package:movie_land/data/models/movie_model.dart';
 import 'package:movie_land/data/repositories/movie_list_repository.dart';
 import 'package:movie_land/data/services/movie_lists_api.dart';
 import 'package:movie_land/presentation/viewmodels/dashboard_view_model.dart';
+import 'package:percent_indicator/circular_percent_indicator.dart';
 import 'package:provider/provider.dart';
 
 class DashboardPage extends StatelessWidget {
@@ -33,13 +35,19 @@ class _DashboardPage extends StatelessWidget {
       appBar: AppBar(title: const Text('Movie Land')),
       body: Consumer<DashboardViewModel>(
         builder:
-            (context, value, child) => PagedListView<int, MovieModel>.separated(
+            (context, value, child) => PagedGridView<int, MovieModel>(
               padding: const EdgeInsets.all(MovieLandDimentions.spaceDefault),
               state: value.state,
               fetchNextPage: value.fetchNextPage,
-              separatorBuilder:
-                  (context, index) =>
-                      SizedBox(height: MovieLandDimentions.spaceDefault),
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                mainAxisSpacing: MovieLandDimentions.spaceDefault,
+                crossAxisSpacing: MovieLandDimentions.spaceDefault,
+                childAspectRatio: 0.45,
+              ),
+              // separatorBuilder:
+              //     (context, index) =>
+              //         SizedBox(height: MovieLandDimentions.spaceDefault),
               builderDelegate: PagedChildBuilderDelegate<MovieModel>(
                 itemBuilder:
                     (context, item, index) => Container(
@@ -62,59 +70,91 @@ class _DashboardPage extends StatelessWidget {
                         mainAxisSize: MainAxisSize.min,
                         crossAxisAlignment: CrossAxisAlignment.stretch,
                         children: [
-                          ClipRRect(
-                            borderRadius: const BorderRadius.only(
-                              topLeft: Radius.circular(8),
-                              topRight: Radius.circular(8),
-                            ),
-                            child: CachedNetworkImage(
-                              imageUrl:
-                                  'https://image.tmdb.org/t/p/w220_and_h330_face${item.posterPath}',
-                              placeholder:
-                                  (context, url) => const Center(
-                                    child: CircularProgressIndicator(),
+                          AspectRatio(
+                            aspectRatio: 2 / 3,
+                            child: Stack(
+                              clipBehavior: Clip.none,
+                              children: [
+                                ClipRRect(
+                                  borderRadius: const BorderRadius.only(
+                                    topLeft: Radius.circular(8),
+                                    topRight: Radius.circular(8),
                                   ),
-                              fit: BoxFit.cover,
+                                  child: CachedNetworkImage(
+                                    imageUrl:
+                                        'https://image.tmdb.org/t/p/w220_and_h330_face${item.posterPath}',
+                                    placeholder:
+                                        (context, url) => const Center(
+                                          child: CircularProgressIndicator(),
+                                        ),
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
+                                Positioned(
+                                  bottom: -25,
+                                  left: 10,
+                                  child: Container(
+                                    width: 50,
+                                    height: 50,
+                                    decoration: BoxDecoration(
+                                      color: Colors.black,
+                                      borderRadius: BorderRadius.circular(100),
+                                    ),
+                                    child: Center(
+                                      child: CircularPercentIndicator(
+                                        radius: 22,
+                                        lineWidth: 3,
+                                        percent: item.voteAverage / 10,
+                                        center: Text(
+                                          '${(item.voteAverage * 10).toStringAsFixed(0)}%',
+                                          style: const TextStyle(
+                                            color: Colors.white,
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 12,
+                                          ),
+                                        ),
+                                        progressColor:
+                                            item.voteAverage >= 7
+                                                ? Colors.green
+                                                : item.voteAverage >= 4
+                                                ? Colors.orange
+                                                : Colors.red,
+                                        backgroundColor: Colors.grey.shade800,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ],
                             ),
                           ),
-                          Text(
-                            item.title,
-                            style: Theme.of(context).textTheme.headlineMedium,
+                          SizedBox(height: MovieLandDimentions.spaceXLarge),
+                          Expanded(
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text(
+                                item.title,
+                                style: Theme.of(context).textTheme.titleSmall,
+                                overflow: TextOverflow.ellipsis,
+                                maxLines: 2,
+                              ),
+                            ),
+                          ),
+                          SizedBox(height: MovieLandDimentions.spaceSmall),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(
+                              DatetimeConverter().convertToHumanReadable(
+                                item.releaseDate,
+                              ),
+                              style: Theme.of(context).textTheme.bodySmall,
+                              overflow: TextOverflow.ellipsis,
+                              maxLines: 2,
+                            ),
                           ),
                         ],
                       ),
                     ),
               ),
-
-              // controller: read.pagingController,
-              // builder:
-              //     (context, state, fetchNextPage) => Container(
-              //       decoration: BoxDecoration(
-              //         color: Colors.white,
-              //         borderRadius: BorderRadius.circular(8),
-              //         boxShadow: [
-              //           BoxShadow(
-              //             color: Colors.grey.withValues(alpha: 0.5),
-              //             spreadRadius: 2,
-              //             blurRadius: 5,
-              //             offset: const Offset(
-              //               0,
-              //               3,
-              //             ), // changes position of shadow
-              //           ),
-              //         ],
-              //       ),
-              //       child: Column(
-              //         mainAxisSize: MainAxisSize.min,
-              //         crossAxisAlignment: CrossAxisAlignment.stretch,
-              //         children: [
-              //           Text(
-              //             'Movies',
-              //             style: Theme.of(context).textTheme.headlineMedium,
-              //           ),
-              //         ],
-              //       ),
-              //     ),
             ),
       ),
     );
